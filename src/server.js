a6c157937a463ca1352b20d85991185dcfb1632c
griiin <julien.nigri@gmail.com>
2014-08-12 12:28:00 +0200

    server connecter

--- src/server.js
+++ src/server.js
@@ -1,148 +1,34 @@
 define(['lodash',
 'q',
 'log',
-'localStorage',
-'rl',
-'socket.io-client'],
-function (_, Q, log, localStorage, rl, socketIO) {
-  var _serverUrl;
-  var _serverPort;
+'serverConnecter'],
+function (_, Q, log, serverConnecter) {
   var _socket;
 
-  var serverConnecter = function () {
+  var server = function () {
 
   };
 
-  serverConnecter.prototype.getSocket = function () {
-    return _socket;
-  };
-
-  serverConnecter.prototype.connectAsync = function () {
+  server.prototype.connectAsync = function () {
     var deferred = Q.defer();
 
-    getServerUrlAsync()
-    .then(getServerPortAsync)
-    .then(connect)
-    .then(saveServerUrlAsync)
-    .then(saveServerPortAsync)
+    serverConnecter.connectAsync()
     .then(function () {
+      _socket = serverConnecter.getSocket();
       deferred.resolve();
     });
 
     return deferred.promise;
   };
 
-  function connect() {
-    var deferred = Q.defer();
+  server.prototype.listen
 
-    var addr = _serverUrl + ":" + _serverPort;
-    log.info('Connection to "' + addr + '" server...');
-    _socket = socketIO.connect(addr, {
-      'reconnection delay' : 0,
-      'reopen delay' : 0,
-      'force new connection' : true
-    });
-    _socket.on("connect", function () {
-      log.info("Done!");
-      return deferred.resolve();
-    });
-
-    return deferred.promise;
-  }
-
-  function saveServerUrlAsync() {
-    var deferred = Q.defer();
-
-    localStorage.saveAsync("serverUrl", _serverUrl)
-    .then(function () {
-      deferred.resolve();
-    })
-    .done();
-
-    return deferred.promise;
-  }
-
-  function saveServerPortAsync() {
-    var deferred = Q.defer();
-
-    localStorage.saveAsync("serverPort", _serverPort)
-    .then(function () {
-      deferred.resolve();
-    })
-    .done();
-
-    return deferred.promise;
-  }
-
-  function getServerPortAsync() {
-    var deferred = Q.defer();
-
-    localStorage.getAsync("serverPort")
-    .then(askServerPortAsync)
-    .then(function () {
-      deferred.resolve();
-    })
-
-    return deferred.promise;
-  }
-
-  function askServerPortAsync(value) {
-    var deferred = Q.defer();
-
-    if (!value) {
-      rl.question("What is the server port? [4242]", function (input) {
-        if (!input || input.length === 0) {
-          _serverPort = '4242';
-        } else {
-          _serverPort = input;
-        }
-        deferred.resolve();
-      });
-    } else {
-      _serverPort = value;
-      deferred.resolve();
-    }
-
-    return deferred.promise;
-  }
-
-  function getServerUrlAsync() {
-    var deferred = Q.defer();
-
-    localStorage.getAsync("serverUrl")
-    .then(askServerUrlAsync)
-    .then(function () {
-      deferred.resolve();
-    });
-
-    return deferred.promise;
-  }
-
-  function askServerUrlAsync(value) {
-    var deferred = Q.defer();
-
-    if (!value) {
-      rl.question("What is the server url? [http://localhost]", function (input) {
-        if (!input || input.length === 0) {
-          _serverUrl = 'http://localhost';
-        } else {
-          _serverUrl = input;
-        }
-        deferred.resolve();
-      });
-    } else {
-      _serverUrl = value;
-      deferred.resolve();
-    }
-
-    return deferred.promise;
-  }
 
   // return singleton
   var instance;
   return (function () {
     if (!instance) {
-      instance = new serverConnecter();
+      instance = new server();
     }
     return instance;
   })();
